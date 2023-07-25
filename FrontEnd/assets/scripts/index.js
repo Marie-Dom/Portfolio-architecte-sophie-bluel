13be5818dc8432c2c03f1070435dac19367ae23a
Marie-Dominique JAOUEN <md.friot@gmail.com>
2023-07-25 11:28:47 +0200

    updating index.html and css's file, adding filters' buttons and functions in index.js

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,10 +1,14 @@
 // Récupération des travaux sur API
+let works;
+let worksReal;
+let idSet = 0;
+let categoriesInMemory;
 
-async function fetchWorks() {
-  await fetch("http://localhost:5678/api/works")
+async function fetchCategories() {
+  await fetch("http://localhost:5678/api/categories")
     .then(async (resp) => {
       if (resp.status == 200) {
-        genererWorks(await resp.json());
+        genererCategories(await resp.json());
       } else {
         console.log("Pas authorisé");
       }
@@ -15,6 +19,25 @@ async function fetchWorks() {
       }
     });
 }
+
+async function fetchWorks() {
+  await fetch("http://localhost:5678/api/works")
+    .then(async (resp) => {
+      if (resp.status == 200) {
+        worksReal = genererWorks(await resp.json());
+        works = worksReal;
+        fetchCategories();
+      } else {
+        console.log("Pas authorisé");
+      }
+    })
+    .catch((err) => {
+      if (!err.data?.message) {
+        console.log("Le site n'arrive pas à communiquer avec le serveur");
+      }
+    });
+}
+
 // Fonction pour la déserialisation de la réponse reçue
 
 function genererWorks(works) {
@@ -38,6 +61,44 @@ function genererWorks(works) {
 
     figure.appendChild(figcaption);
   }
+  return works;
+}
+
+function cleanFigure() {
+  let figure = document.querySelectorAll("figure");
+  for (let i = 1; i < figure.length; i++) {
+    figure[i].remove();
+  }
 }
 
 fetchWorks();
+
+// Ajout des catégories avec les boutons filtres
+
+const filterBar = document.querySelector(".filterBar");
+
+function genererCategories(category) {
+  const objectTous = { id: 0, name: "Tous" };
+  const categories = [objectTous];
+  categories.push(...category);
+  categoriesInMemory = categories;
+  for (let i = 0; i < categories.length; i++) {
+    const filterButton = document.createElement("button");
+    filterButton.innerText = categories[i].name;
+    filterButton.classList.add("filterButton");
+    filterBar.appendChild(filterButton);
+    filterButton.addEventListener("click", () => {
+      let cat = categoriesInMemory.find(
+        (element) => element.name === filterButton.innerText
+      );
+      idSet = cat.id;
+      if (idSet != 0) {
+        works = worksReal.filter((work) => work.categoryId === idSet);
+      } else {
+        works = worksReal;
+      }
+      cleanFigure();
+      genererWorks(works);
+    });
+  }
+}
